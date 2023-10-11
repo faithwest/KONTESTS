d2eef94f6b2e63e570abb8325311ba9e5d94574e
faithwest <faithchepkoech425@gmail.com>
2023-10-11 11:39:58 +0300

    updating the first table

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const apiUrl = "https://kontests.net/api/v1/all";
+    const futureContestsContainer = document.getElementById("future-contests");
+    const currentContestsContainer = document.getElementById("current-contests"); // Updated variable name
 
     // Function to fetch contest data
     function fetchContests() {
@@ -7,54 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(response => response.json())
             .then(data => {
                 const contests = data.contests;
-                // Display contests in the table
-                displayContests(contests);
+
+                const futureContests = contests.filter(contest => isFutureContest(contest));
+                const currentContests = contests.filter(contest => !isFutureContest(contest)); // Updated variable name
+
+                // Display contests in the respective tables
+                displayContests(futureContests, futureContestsContainer);
+                displayContests(currentContests, currentContestsContainer); // Updated variable name
             })
             .catch(error => console.error('Error:', error));
     }
 
-    // Function to display contests in the table
-    function displayContests(contests) {
-        const table = document.querySelector("table");
-        table.innerHTML = ""; // Clear the table
+    // ... (rest of the code remains the same)
 
-        for (const contest of contests) {
-            const row = table.insertRow();
-
-            // Add columns for each contest property
-            const nameCell = row.insertCell(0);
-            nameCell.textContent = contest.name;
-
-            const startTimeCell = row.insertCell(1);
-            startTimeCell.textContent = contest.start_time;
-
-            const endTimeCell = row.insertCell(2);
-            endTimeCell.textContent = contest.end_time;
-
-            const durationCell = row.insertCell(3);
-            durationCell.textContent = contest.duration;
-
-            const siteCell = row.insertCell(4);
-            siteCell.textContent = contest.site;
-        }
-    }
-
-    // Add an event listener to fetch contests when the page loads
-    fetchContests();
-
-    // "Next" button
+    // Event listener for the "Next" button
     document.querySelector(".next-button").addEventListener("click", function () {
         // Implement code to load the next page of contests
         // You can use pagination from the API if available
     });
 
-    // "Previous" button
+    // Event listener for the "Previous" button
     document.querySelector(".previous-button").addEventListener("click", function () {
         // Implement code to load the previous page of contests
         // You can use pagination from the API if available
     });
 
-    // "Submit" button
+    // Event listener for the "Submit" button
     document.querySelector(".search-button").addEventListener("click", function () {
         const inputText = document.querySelector(".input-text").value;
         // Implement code to search contests based on the inputText
